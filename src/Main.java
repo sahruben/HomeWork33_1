fc0d77777c68a6768ddf3b2f082244ddee0e7ec7
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2023-12-28 20:49:35 +0100

    add Main class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sum = Sum.sum(array);
+
+        System.out.println(sum); // 45
+    }
+}
+
